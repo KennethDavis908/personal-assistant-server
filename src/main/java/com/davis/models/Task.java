5139d3c6aec8ad961e57dba7c743b0a6196513f5
Kenny Davis <kennethdavis391@gmail.com>
2023-10-12 11:12:04 -0400

    Task model created

---
+++ src/main/java/com/davis/models/Task.java
@@ -0,0 +1,31 @@
+package com.davis.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Tasks will store items on ToDo lists
+ *
+ * @author Kenneth Davis
+ *
+ */
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "task")
+public class Task {
+
+    @Id
+    @Column(name = "task_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "complete", nullable = false)
+    private boolean complete;
+}
