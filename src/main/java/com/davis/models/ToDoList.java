27c5ca93549c0d47d856004e8f0e6c139420d7c2
Kenny Davis <kennethdavis391@gmail.com>
2023-10-12 11:36:48 -0400

    ToDoList model created

---
+++ src/main/java/com/davis/models/ToDoList.java
@@ -0,0 +1,37 @@
+package com.davis.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ * ToDoLists will house tasks for a particular day
+ *
+ * @author Kenneth Davis
+ *
+ */
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "to_do_list")
+public class ToDoList {
+
+    @Id
+    @Column(name = "to_do_list_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "created_on")
+    @CreationTimestamp
+    private LocalDate createdOn;
+
+    @OneToMany(fetch = FetchType.EAGER)
+    @JoinColumn(name = "to_do_list_id")
+    private List<Task> tasks;
+}
