89d96d5c14d00b41c773bffded757c683b963023
KennethDavis908 <80171975+KennethDavis908@users.noreply.github.com>
2024-01-02 13:55:25 -0500

    PAA-012 Auth0 & Spring Security

---
+++ src/main/java/com/davis/auth/CorsConfigurationProps.java
@@ -0,0 +1,21 @@
+package com.davis.auth;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "cors")
+public class CorsConfigurationProps {
+
+    private List<String> allowedOrigins;
+    private List<String> allowedMethods;
+    private List<String> allowedHeaders;
+    private List<String> exposedHeaders;
+    private boolean allowCredentials;
+}
